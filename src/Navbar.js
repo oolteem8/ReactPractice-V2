64017be71b8f75eac1e64ece88adc1aa3d2dbaaf
oolteem8 <91712002+oolteem8@users.noreply.github.com>
2023-10-02 19:22:23 +0800

    Misc

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,15 +2,11 @@ export default function Navbar() {
     return (
         <header>
             <nav className="nav-items">
-                <div>
+                <div class="img-wrap">
                     <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"} alt="Logo" />
                 </div>
-
-                <ul className="unordered-list">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+                <h1 class="header-title">ReactFacts</h1>
+                <h3 class="sub-title">React Course - Project 1</h3>
             </nav>
         </header>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { StrictMode } from "react"
 import { React } from "react"
 import { createRoot } from "react-dom/client"
-import Header from "./Header"
 import Navbar from "./Navbar"
+import Header from "./Header"
 import MainContent from "./MainContent"
-import Footer from "./Footer"
 import "./style.css"
 // import "./style.css"
 // import ReactLogo from "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
@@ -14,10 +13,9 @@ const root = createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
         <div>
-            <Header />
             <Navbar />
+            <Header />
             <MainContent />
-            <Footer />
         </div>
     </StrictMode>
 );
