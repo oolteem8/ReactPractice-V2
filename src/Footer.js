b09be2e47f374373f8989d6ac3e787654a66a6f3
oolteem8 <91712002+oolteem8@users.noreply.github.com>
2023-10-03 09:34:22 +0800

    Deleted Footer.js

--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="footer-container">
-            <p>©oolteem8 development. All rights reserved.</p>
-        </div>
-    );
-}
