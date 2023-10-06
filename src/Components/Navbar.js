02b013afb27fec732c378ddab1690f1d1b0ae267
oolteem8 <91712002+oolteem8@users.noreply.github.com>
2023-10-06 11:09:16 +0800

    Final Commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,11 @@
 export default function Navbar() {
     return (
-        <header class="navbar">
-            <nav className="nav-items">
-                <div class="img-wrap">
-                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"} alt="Logo" />
-                </div>
-                <h1 class="header-title">ReactFacts</h1>
-                <h3 class="header-subtitle">React Course - Project 1</h3>
-            </nav>
-        </header>
+        <nav className="nav-items">
+            <div class="img-wrap">
+                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"} alt="Logo" />
+            </div>
+            <h1 class="header-title">ReactFacts</h1>
+            <h3 class="header-subtitle">React Course - Project 1</h3>
+        </nav>
     );
 }
